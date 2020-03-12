ea2647ea2fa85bc5c57106e178cb6921e882d07b
Kashyap Patel <kaspatel@paloaltonetworks.com>
2020-03-12 01:09:30 -0700

    task 3,4,5

--- test.py
+++ test.py
@@ -5,12 +5,19 @@ from pprint import *
 import sys
 import requests 
 import json
+l = []
+parser = argparse.ArgumentParser(description='Json parser')
+parser.add_argument('-s',action="store",dest="session_id",default=107,metavar="<session id>", type = int,
+	help= "Provide an integer to get status by session id")
+parser.add_argument('-d',action="store",dest="device_id",default="3",metavar="<device id>", type = int,
+	help= "Provide an integer to get status by device id")
 
-# parser = argparse.ArgumentParser(description='Json parser')
-# parser.add_argument('string',metavar="<provide url here>", help= "Argv1 = provide url to parse")
-# args = parser.parse_args()
-
+args = parser.parse_args()
+# print (args.device_id)
+# print (args.session_id)
 baseurl = "https://transit-sessions.s3-us-west-2.amazonaws.com/sessions.json"
+
+### function to do an API call to the give url 
 
 def parser(url):
 	req = requests.get(url)
@@ -24,11 +31,43 @@ print ("\n#####################Task1 Output#########################\n")
 pprint (output) 
 
 print ("\n#####################Task2 Output#########################\n")
-# print type(output)
-for element in output:
-	if element["id"] == 107:
-		pprint (element)
+# # print type(output)
+# for element in output:
+# 	if element["id"] == 107:
+# 		pprint (element)
+def session_id(id_number):
+	for element in output:
+		if element["id"] == int(id_number):
+			return element
+pprint (session_id(107))
 #### Task 2 can be also achievable by below ###################
 # pprint (output[6])
+print ("\n#####################Task3 Output session id =107#########################\n")
+def getstatus(dict):
+	if int(dict['status']) == 0:
+		print ("OK")
+	else:
+		print ("CRITICAL")
 
+getstatus(session_id(107))	
+# if session_id(107)['status'] == 0:
+# 	print "OK"
+# else:
+# 	print "CRITICAL"
 
+print ("\n#####################Task4 Output device id = 3#########################\n")
+
+def getstatusbydeviceid(deviceid):
+	l=[]
+	for element in output:
+		if element["device_id"] == str(deviceid):
+			getstatus(element)			
+
+getstatusbydeviceid(3)
+
+print ("\n#####################Task5 Output by session id = %s#########################\n"%(args.session_id))
+
+getstatus(session_id(args.session_id))
+
+print ("\n#####################Task5 Output by device id = %s #########################\n"%(args.device_id))
+getstatusbydeviceid(args.device_id)
