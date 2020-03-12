0a27bd8dee23ba0759f01173862555da31d14404
Kashyap Patel <kaspatel@paloaltonetworks.com>
2020-03-11 18:01:31 -0700

    first commit

---
+++ test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3 
+
+import argparse 
+from pprint import *
+import sys
+import requests 
+
+parser = argparse.ArgumentParser(description='Json parser')
+parser.add_argument('string',metavar="<url>", help= "Argv1 = provide url to parse")
+args = parser.parse_args()
+
+baseurl = str(sys.argv[1])
+# baseurl = "https://transit-sessions.s3-us-west-2.amazonaws.com/sessions.json"
+def parser(url):
+	req = requests.get(url)
+	return req.content
+
+
+pprint (parser(baseurl))
